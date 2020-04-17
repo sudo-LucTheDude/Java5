c78a78d3fd06b63d2dd93c9a03a7adb9cd4cea6f
moselerl <moselerl@ict-campus.net>
2020-04-17 14:39:41 +0200

    unnötige For Schleife

--- src/Hallo.java
+++ src/Hallo.java
@@ -1,6 +1,9 @@
 public class Hallo {
     public static void main(String[] args) {
-        System.out.print("Moin");
+        System.out.println("Moin");
         System.out.println("testnachricht");
+        for(int i = 0; i<100;i++){
+            System.out.println(i);
+        }
     }
 }
