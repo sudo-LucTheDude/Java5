e937ffaa6850d3e43cbb8086f4578b28b705986d
vonwerdtt <try776>
2020-04-22 11:03:29 +0200

    out erstellt

--- src/Hallo.java
+++ src/Hallo.java
@@ -1,8 +1,9 @@
 public class Hallo {
+
     public static void main(String[] args) {
         System.out.println("Moin");
         System.out.println("testnachricht");
-        for(int i = 0; i<100;i++){
+        for (int i = 0; i < 100; i++) {
             System.out.println(i);
         }
     }
