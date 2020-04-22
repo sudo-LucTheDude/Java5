a2f71ef7adc01f484a0e6c7fe8a4a4c0b70d8b64
vonwerdtt <try776>
2020-04-22 11:34:19 +0200

    fx hinzugefügt (unstable)

---
+++ src/Database/DBconnection.java
@@ -0,0 +1,57 @@
+package Database;
+import JavaFX.Login;
+
+import java.sql.*;
+
+public class DBconnection {
+
+    private String username;
+    private String password;
+
+    public void pw(){
+        String url ="jdbc:mysql://localhost:3306/login?useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Europe/Berlin";
+        String user = "root";
+        String password = "1234";
+
+        Login lg = new Login();
+
+        String uname = getUsername();
+
+        try(Connection conn = DriverManager.getConnection(url,user,password)){
+            System.out.println("Erfolgreich mit Datenbank verbunden!");
+
+            //Ausgeben
+            String query = "Select password from data where username ='" + uname +"'";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            int col = rs.getMetaData().getColumnCount();
+
+            while(rs.next()){
+                for(int i = 1; i <= col; i++){
+                    setPassword(String.format(rs.getString(i)));
+                }
+            }
+
+        }catch(SQLException ex){
+            System.err.println(ex.getMessage());
+        }
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ src/JavaFX/Login.java
@@ -0,0 +1,124 @@
+package JavaFX;
+
+import Database.DBconnection;
+import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import javax.print.attribute.standard.PresentationDirection;
+import java.awt.*;
+import java.util.concurrent.ExecutionException;
+
+public class Login extends Application  {
+    Button btnLogin1;
+    Label lblTitle1;
+    Label lblUsername1;
+    Label lblPassword1;
+    Label lblFail1;
+    PasswordField txtPassword1;
+    TextField txtUsername1;
+
+    Button btnBack2;
+    Label lblTitle2;
+    Label lblContent2;
+    Stage window;
+    Scene scene1,scene2;
+
+
+    @Override
+    public void start(Stage PrimaryStage) throws Exception {
+
+        DBconnection db = new DBconnection();
+        window = PrimaryStage;
+
+
+        GridPane layout1 = new GridPane();
+        GridPane layout2 = new GridPane();
+
+        btnLogin1 = new Button("Login");
+        btnLogin1.setFont(new Font(15));
+
+        /*
+        btnLogin1.setOnAction(e -> {
+
+            db.setUsername(txtUsername1.getText());
+            db.pw();
+            if(txtPassword1.getText().equals(db.getPassword())) {
+                window.setScene(scene2);
+
+            }else{
+                layout1.add(lblFail1, 1,4);
+            }
+        });
+        */
+
+        lblFail1 = new Label("Falsches Passwort");
+        lblFail1.setFont(new Font(10));
+        lblFail1.setStyle("-fx-background-color: red");
+        lblTitle1 = new Label("Loginformular");
+        lblTitle1.setFont(new Font(20));
+        lblTitle1.setPadding(new Insets(10));
+
+        lblUsername1 = new Label("Username");
+        lblUsername1.setPadding(new Insets(10));
+
+        lblPassword1 = new Label("Password");
+        lblPassword1.setPadding(new Insets(10));
+
+        txtUsername1 = new TextField();
+        txtUsername1.setPromptText("Username...");
+        txtUsername1.setPadding(new Insets(10));
+
+        txtPassword1 = new PasswordField();
+        txtPassword1.setPromptText("Password...");
+        txtPassword1.setPadding(new Insets(10));
+        scene1 = new Scene(layout1, 300,200);
+
+
+        layout1.setPadding(new Insets(10));
+        layout2.setPadding(new Insets(10));
+
+        lblContent2 = new Label("Herzlich willkommen User!");
+        lblContent2.setPadding(new Insets(10));
+        lblTitle2 = new Label("Interner Bereich");
+        lblTitle2.setFont(new Font(20));
+        lblTitle2.setPadding(new Insets(10));
+        scene2 = new Scene(layout2, 300,200);
+        btnBack2 = new Button("Logout");
+
+        //btnBack2.setOnAction(e -> window.setScene(scene1));
+
+        layout1.add(lblTitle1 ,0,0, 2,1);
+        layout1.add(lblUsername1,0,1);
+        layout1.add(txtUsername1,1,1);
+        layout1.add(lblPassword1,0,2);
+        layout1.add(txtPassword1,1,2);
+        layout1.add(btnLogin1,1,3);
+        //layout1.add(lblfail,1,4);
+
+        layout2.add(lblTitle2,1,1);
+        layout2.add(lblContent2, 1, 3);
+        layout2.add(btnBack2,2,5);
+
+
+        window.setScene(scene1);
+        window.setTitle("HalloWelt");
+        window.show();
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+
+}
