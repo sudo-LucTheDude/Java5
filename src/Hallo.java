0f534c7ff3f23616d604b8b8a50bd4e1b2e29c19
moselerl <moselerl@ict-campus.net>
2020-04-22 15:46:11 +0200

    code++

--- src/Hallo.java
+++ src/Hallo.java
@@ -3,7 +3,7 @@ public class Hallo {
     public static void main(String[] args) {
         System.out.println("Moin");
         System.out.println("testnachricht");
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10000000; i++) {
             System.out.println(i);
         }
     }
---
+++ src/socket/client/SwsClient.java
@@ -0,0 +1,35 @@
+package socket.client;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class SwsClient {
+private DatagramSocket socket;
+private InetAddress address;
+private int port;
+
+public SwsClient(String address, int port){
+    try {
+        this.address = InetAddress.getByName(address); //Convertiert String-->InetAddress
+        this.port = port;
+        socket = new DatagramSocket();
+
+    }catch(Exception e){
+        e.printStackTrace();
+    }
+}
+
+public void send(String message, InetAddress address, int port){
+    try{
+        message += "\\e";
+        byte[] data = message.getBytes(); //konvertiert String zu Byte[]
+        DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
+        socket.send(packet);
+        System.out.println("Nachricht an"+ address.getHostAddress() + ":"+port+" gesendet");
+    }catch(Exception e){
+        e.printStackTrace();
+    }
+}
+
+}
