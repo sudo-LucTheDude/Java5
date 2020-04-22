263e09f8492fcfdc315ed6cef0a2325053528d23
moselerl <moselerl@ict-campus.net>
2020-04-22 14:09:14 +0200

    add SwsServer.listen();

--- src/socket/server/SwsServerActions.java
+++ src/socket/server/SwsServerActions.java
@@ -8,17 +8,20 @@ public class SwsServerActions{
 
     private static DatagramSocket socket; //(UDP-Socket)
     private static int port;
+    private static boolean running;
 
     public static void start(int port){
     try{
         socket = new DatagramSocket(port); //(UDP-Socket)
         System.out.println("Server startet auf Port: " + port);
+        running = true;
+        listen();
     }catch(Exception e){
         e.printStackTrace();
         }
     }
 
-    public static void broadcast(){
+    public static void broadcast(String message){
 
     }
 
@@ -27,7 +30,25 @@ public class SwsServerActions{
     }
 
     public static void listen(){
+        Thread listenThread = new Thread("Einkommende Nachrichten") // Neuer Thread, ansonsten würde die Methode die Applikation nicht weiterlaufen lassen
+        {
+            //run() wird für den Thread benötigt
+            public void run(){
+                try{
+                    while(running){
+                        byte[] data = new byte[1024]; //Byte Array wegen Datagram-Socket/Packet, in jedem Byte wird ein Char gespeichert
+                        DatagramPacket packet = new DatagramPacket(data, data.length); //DatagramPacket enhält viele MetaDaten, bspw. Absender usw.
+                        socket.receive(packet);
+                        String message = new String(data);
+                        message = message.substring(0, message.indexOf("\\e")); //\\e markiert die Letzte !Null stelle des dataArrays
 
+                        broadcast(message);
+                    }
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+    }; listenThread.start();
     }
 
     public static void stop(){
